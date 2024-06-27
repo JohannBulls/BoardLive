9ef4d7715f08250c827d3ad61fde688de0659b6f
Johannbulls <johannpez99@gmail.com>
2024-06-27 12:52:26 -0500

    React tablero p5

---
+++ boardlivefront/src/App.js
@@ -0,0 +1,32 @@
+import { useRef, useEffect } from 'react';
+import p5 from 'p5';
+
+function App() {
+  const sketch = function (p) {
+      let x = 100;
+      let y = 100;
+      p.setup = function () {
+          p.createCanvas(700, 410);
+      }
+      p.draw = function () {
+          if (p.mouseIsPressed === true) {
+              p.fill(0, 0, 0);
+              p.ellipse(p.mouseX, p.mouseY, 20, 20);
+          }
+          if (p.mouseIsPressed === false) {
+              p.fill(255, 255, 255);
+          }
+      }
+  };
+  const myp5 = useRef(new p5(sketch, 'container'));
+  return(
+            <div>
+              <hr/>
+              <div id="container"></div>
+              <hr/>
+            </div>
+        );
+}
+
+export default App;
+
